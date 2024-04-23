287d1efc2c1e75ebd2cf9aba3ff5462f4bcae8cf
fusan <fusanfilm@163.com>
2024-04-23 22:57:25 +0800

    project created

---
+++ src/main/java/com/fullstackspringreact/FullstackSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.fullstackspringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackSpringReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackSpringReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fullstackspringreact/FullstackSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackspringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackSpringReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
